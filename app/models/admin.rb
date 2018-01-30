8634c179173d30c9450e8c0c9c99445c6ad4d0ce
Julianocarvalho <julianocarvalho128@gmail.com>
2018-01-30 20:48:17 -0200

    atualizando commit anterior

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,9 @@
 class Admin < ActiveRecord::Base
 	enum role: {:full_access => 0, :restricted_access => 1}
 
-	scope :with_full_access, -> {where(role: 'full_access')}
+	#scopes
+	scope :with_full_access, -> {where(role: 0)}
+	scope :with_restricted_access, -> {where(role: 1)}
 
 	def self.with_full_access
 		where(role: 'full_access')
